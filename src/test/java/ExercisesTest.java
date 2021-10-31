3aec5125e9cbbdea1ede44ae81fb6c769ea6f1c8
Milton <milton761@gmail.com>
2021-10-31 16:41:44 -0500

    unit test for exercises

---
+++ src/test/java/ExercisesTest.java
@@ -0,0 +1,37 @@
+import exercises.Exercises;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExercisesTest {
+
+    private Exercises exerciseTest = new Exercises();
+
+    @Test
+    public void case_1() {
+        Assert.assertEquals(exerciseTest.isBalanced("{[()]}"), "YES");
+    }
+
+    @Test
+    public void case_2() {
+        Assert.assertEquals(exerciseTest.isBalanced("[(])"), "NO");
+    }
+
+    @Test
+    public void case_3() {
+        Assert.assertEquals(exerciseTest.isBalanced("[[(())]]"), "YES");
+    }
+
+    public void case_4() {
+        Assert.assertEquals(exerciseTest.isBalanced(""), "YES");
+    }
+
+    @Test
+    public void case_5() {
+        Assert.assertEquals(exerciseTest.isBalanced("[[]"), "NO");
+    }
+
+    @Test
+    public void case_6() {
+        Assert.assertEquals(exerciseTest.isBalanced("[[(({}))]]"), "YES");
+    }
+}
